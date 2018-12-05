3c5655b850251ce1f7992a96fb9d8366b92ac3f9
hpyz <2842382609@qq.com>
2018-12-05 16:40:03 +0800

    配置文件

--- connfig/index.js
+++ connfig/index.js
@@ -1,5 +1,24 @@
-const config = {
+const { join } = require('path');
+const { extend } = require('lodash');
 
+let config = {
+    viewDir: join(__dirname, "..", "views"),
+    staticDir: join(__dirname, "..", "public")
+}
+
+if (process.env.NODE_ENV == "development") {
+    const localConfig = {
+        baseUrl: "http://localhost:59793",
+        port: 3000
+    }
+    config = extend(config, localConfig);
+}
+
+if (process.env.NODE_ENV == "production") {
+    const prodConfig = {
+        port: 8081
+    }
+    config = extend(config, prodConfig);
 }
 
 module.exports = config;
